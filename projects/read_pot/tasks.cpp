aea5490d671138c150d28c1f7d8a8184b7ab7d7b
Justin Jordan <jordanj@curtisinst.com>
2022-01-27 22:21:12 -0800

    update read pot to samd21 xplained dev board

--- projects/read_pot/tasks.cpp
+++ projects/read_pot/tasks.cpp
@@ -1,30 +1,24 @@
 
 #include <array>
-#include <cstdio>
 #include "asf.h"
+#include "stdio_serial.h"
+#include "conf_uart_serial.h"
 #include "tasks.h"
-
-/** Configure LED0, turn it off*/
-static void config_led(void)
-{
-	struct port_config pin_conf;
-	port_get_config_defaults(&pin_conf);
-
-	pin_conf.direction  = PORT_PIN_DIR_OUTPUT;
-	port_pin_set_config(PIN_PA09, &pin_conf);
-	port_pin_set_output_level(PIN_PA09, false);
-}
 
 static struct usart_module cdc_uart_module;
 static void configure_console(void)
 {
 	struct usart_config usart_conf;
 
-	//8N1 9600 PA05 is Rx and PA08 is Tx on SAMD10C14
-	//8N1 9600 PA05 is Rx and PA06 is Tx on SAMD21G15B
 	usart_get_config_defaults(&usart_conf);
-	usart_conf.baudrate = 115200;
-	stdio_serial_init(&cdc_uart_module, SERCOM0, &usart_conf);
+	usart_conf.mux_setting = CONF_STDIO_MUX_SETTING;
+	usart_conf.pinmux_pad0 = CONF_STDIO_PINMUX_PAD0;
+	usart_conf.pinmux_pad1 = CONF_STDIO_PINMUX_PAD1;
+	usart_conf.pinmux_pad2 = CONF_STDIO_PINMUX_PAD2;
+	usart_conf.pinmux_pad3 = CONF_STDIO_PINMUX_PAD3;
+	usart_conf.baudrate    = CONF_STDIO_BAUDRATE;
+
+	stdio_serial_init(&cdc_uart_module, CONF_STDIO_USART_MODULE, &usart_conf);
 	usart_enable(&cdc_uart_module);
 }
 
@@ -34,6 +28,7 @@ static void configure_adc(void)
 	struct adc_config config_adc;
 	adc_get_config_defaults(&config_adc);
 	config_adc.reference = ADC_REFERENCE_INTVCC1; //1.65V
+	config_adc.positive_input = ADC_POSITIVE_INPUT_PIN8;
 	adc_init(&adc_instance, ADC, &config_adc);
 	adc_enable(&adc_instance);
 }
@@ -51,10 +46,10 @@ static void configure_tc(void)
 	config_tc.clock_prescaler = TC_CLOCK_PRESCALER_DIV2;
 	
 	config_tc.pwm_channel[0].enabled = true;
-	config_tc.pwm_channel[0].pin_out = PIN_PA18E_TC3_WO0;
-	config_tc.pwm_channel[0].pin_mux = MUX_PA18E_TC3_WO0;
+	config_tc.pwm_channel[0].pin_out = PIN_PB02E_TC6_WO0;
+	config_tc.pwm_channel[0].pin_mux = MUX_PB02E_TC6_WO0;
 
-	tc_init(&tc_instance, TC3, &config_tc);
+	tc_init(&tc_instance, TC6, &config_tc);
 	tc_enable(&tc_instance);
 }
 
@@ -71,7 +66,6 @@ uint32_t get_schedular_ticks_from_us(uint32_t micro_seconds)
 void init_app()
 {
 	system_init();
-	config_led();
 	configure_console();
 	configure_adc();
 	configure_tc();
