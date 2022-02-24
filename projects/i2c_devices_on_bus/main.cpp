fd1da5a134d9b216faf444b0a502e5565546fe05
Justin Jordan <jordanj@curtisinst.com>
2022-02-24 15:18:47 -0800

    i2c find devices on bus

--- projects/i2c_devices_on_bus/main.cpp
+++ projects/i2c_devices_on_bus/main.cpp
@@ -20,24 +20,54 @@ static void configure_console(void)
 	usart_enable(&cdc_uart_module);
 }
 
+/* Number of times to try to send packet if failed. */
+#define TIMEOUT 1000
+
+struct i2c_master_module i2c_master_instance;
+
+void configure_i2c_master(void)
+{
+	/* Initialize config structure and software module. */
+	struct i2c_master_config config_i2c_master;
+	i2c_master_get_config_defaults(&config_i2c_master);
+	
+	/* Change buffer timeout to something longer. */
+	config_i2c_master.buffer_timeout = 10000;
+
+	/* Initialize and enable device with config. */
+	i2c_master_init(&i2c_master_instance, SERCOM2, &config_i2c_master);
+
+	i2c_master_enable(&i2c_master_instance);
+	
+}
+
 int main(void)
 {
 	system_init();
 	delay_init();
 	configure_console();
+	configure_i2c_master();
 
 	printf("Hello World!\r\n");
 
-	uint8_t my_char {0};
+	i2c_master_packet test_packet;
+	test_packet.data_length = 0;
+	test_packet.ten_bit_address = false;
+	test_packet.high_speed = false;
 
-	while (true) 
+	for(uint8_t adrs = 0; adrs < 0x80; adrs++)
 	{
-		usart_serial_getchar(&cdc_uart_module, &my_char);
-		usart_serial_putchar(&cdc_uart_module, my_char);
-
+		printf("Trying slave address %d\r\n", adrs);
+		test_packet.address = adrs;
+		status_code i2c_status = i2c_master_write_packet_wait(&i2c_master_instance, &test_packet);
+		if(i2c_status == STATUS_OK)
+		{
+			printf("Found device at %d\r\n", adrs);
+		}
 		LED_Toggle(LED_0_PIN);
 		delay_ms(100);
-		LED_Toggle(LED_0_PIN);
 	}
+
+	while (true);
 }
 
