a3e0a5bb4869979d2b042025386316dbc77882dd
Justin Jordan <jordanj@curtisinst.com>
2022-09-24 10:35:26 -0700

    rename folder

---
+++ scheduler/Task.h
@@ -0,0 +1,106 @@
+/******************************************************************************
+* MIT License
+* 
+* Copyright (c) 2021 Justin J. Jordan
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+******************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+/// Simple cooperative scheduler
+namespace Schedular
+{
+    /// Class for creating a task
+    class Task
+    {
+        public:
+
+            /// Maximum length of task name
+            static constexpr uint32_t max_task_name_length {32};
+
+			/// Configuration of task
+            struct Config
+            {
+                uint32_t startTick;
+                uint32_t stopTick;
+                uint32_t lastStartTick;
+                uint32_t executionTime;
+                uint32_t interval;
+                void(*execute)();
+            };
+
+			/// Status of task
+            enum class Status
+            {
+                idle = 0,
+                running,
+                late_start,
+                exceeded_runtime
+            };
+
+			/**
+			* @brief Default constructor
+			*/
+            Task() = default;
+
+			/**
+			* @brief Task constructor
+			*
+			* @param[in] name - sting object name of task
+			* @param[in] interval - interval at which task should be run in
+			* terms of scheduler ticks
+			* @param[in] execute - pointer to function implementing task
+			*/
+            Task(std::string& name, uint32_t interval, void(*execute)());
+
+			/**
+			* @brief Task constructor
+			*
+			* @param[in] name - C string name of task
+			* @param[in] interval - interval at which task should be run in
+			* terms of scheduler ticks
+			* @param[in] execute - pointer to function implementing task
+			*/
+            Task(const char *  name, uint32_t interval, void(*execute)());
+
+			/**
+			* @brief Gets task configuration
+			*
+			* @return configuration of task
+			*/
+            Config& get_config();
+
+			/**
+			* @brief Runs task execute function
+			*
+			* @param[in] start_tick - scheduler tick at which task was started
+			*
+			* @return status of task
+			*/
+            Status run(uint32_t start_tick);
+
+        private:
+            std::string name;
+            Config config;
+    };
+}//namespace Schedular
