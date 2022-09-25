eb841723641b3187d49be7967e6af930673d2585
Justin Jordan <jordanj@curtisinst.com>
2022-09-25 12:24:42 -0700

    simple blinky project building

--- projects/blinky/main.cpp
+++ projects/blinky/main.cpp
@@ -1,18 +1,14 @@
 
-#include "asf.h"
+#include <cstddef>
+#include <cstdint>
 
 int main(void)
 {
-	system_init();
-	delay_init();
-
-	bool button_pressed {false};
+	size_t idx {0};
 
 	while (true) 
 	{
-		button_pressed = !port_pin_get_input_level(BUTTON_0_PIN);
-		button_pressed ? delay_ms(125) : delay_ms(500);
-		LED_Toggle(LED_0_PIN);
+		idx++; 
 	}
 }
 
