28e10113b1fed39ec52da905bf6d52ef1de9b92d
Justin Jordan <justinjordan1978@gmail.com>
2024-01-24 16:04:52 -0800

    remove 'asf' from project names

--- projects/blinky/main.cpp
+++ projects/blinky/main.cpp
@@ -1,14 +1,18 @@
 
-#include <cstddef>
-#include <cstdint>
+#include "asf.h"
 
 int main(void)
 {
-	size_t idx {0};
+	system_init();
+	delay_init();
+
+	bool button_pressed {false};
 
 	while (true) 
 	{
-		idx++; 
+		button_pressed = !port_pin_get_input_level(BUTTON_0_PIN);
+		button_pressed ? delay_ms(125) : delay_ms(500);
+		LED_Toggle(LED_0_PIN);
 	}
 }
 
