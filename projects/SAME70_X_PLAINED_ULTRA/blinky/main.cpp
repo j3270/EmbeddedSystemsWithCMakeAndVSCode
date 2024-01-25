505ea7d6bf2bc857e915e76cf2c15a413a9cf4fc
Justin Jordan <justinjordan1978@gmail.com>
2024-01-24 16:41:55 -0800

    Add subdirectory for boards I use

---
+++ projects/SAME70_X_PLAINED_ULTRA/blinky/main.cpp
@@ -0,0 +1,18 @@
+
+#include "asf.h"
+
+int main(void)
+{
+	system_init();
+	delay_init();
+
+	bool button_pressed {false};
+
+	while (true) 
+	{
+		button_pressed = !port_pin_get_input_level(BUTTON_0_PIN);
+		button_pressed ? delay_ms(125) : delay_ms(500);
+		LED_Toggle(LED_0_PIN);
+	}
+}
+
